958140f556a59168c0e6d0f1bd63de31592d759e
faust <me@fau.dev>
2024-01-05 22:43:29 -0500

    add caliligos and achates

--- src/lib/utils/sync.ts
+++ src/lib/utils/sync.ts
@@ -39,7 +39,9 @@ export const bosses = [
     "Sonavel",
     "Hanumatan",
     "Kungelanium",
-    "Deskaluda"
+    "Deskaluda",
+    "Caliligos",
+    "Achates"
 ];
 
 export async function uploadLog(id: string, encounter: Encounter, settings: any, auto = false) {
